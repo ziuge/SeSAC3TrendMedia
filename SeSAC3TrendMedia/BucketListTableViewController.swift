3e31ac90b3ebb8fbc9252c2f67633c6bde64c584
ziuge <xwcjwwd@ewhain.net>
2022-07-19 16:00:49 +0900

    0719 수업 코드

--- SeSAC3TrendMedia/BucketListTableViewController.swift
+++ SeSAC3TrendMedia/BucketListTableViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class BucketListTableViewController: UITableViewController {
     
+    @IBOutlet weak var userTextField: UITextField!
     var list = ["범죄도시2", "탑건", "토르"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.rowHeight = 80
+        
+        list.append("마녀")
+        list.append("aaa")
+    }
+    
+    @IBAction func userTextFieldFinished(_ sender: UITextField) {
+        list.append(userTextField.text!)
+        
+        // 테이블 뷰 다시 그려줘
+        tableView.reloadData()
+//        tableView.reloadSections(<#T##sections: IndexSet##IndexSet#>, with: <#T##UITableView.RowAnimation#>)
+//        tableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,5 +43,17 @@ class BucketListTableViewController: UITableViewController {
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            // 배열 삭제 후 테이블뷰 갱신
+            list.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
 }
