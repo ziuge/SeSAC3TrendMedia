ec0fc93d312ad2b359c30e395ec48e621b774dcc
ziuge <xwcjwwd@ewhain.net>
2022-07-20 14:56:39 +0900

    0720 수업 코드 완료

---
+++ SeSAC3TrendMedia/Trend/RecommandCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecommandCollectionViewCell.swift
+//  SeSAC3TrendMedia
+//
+//  Created by CHOI on 2022/07/20.
+//
+
+import UIKit
+
+class RecommandCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImgView: UIImageView!
+}
--- SeSAC3TrendMedia/Trend/RecommandCollectionViewController.swift
+++ SeSAC3TrendMedia/Trend/RecommandCollectionViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import Toast
+import Kingfisher
+
+/*
+    
+ */
 
 class RecommandCollectionViewController: UICollectionViewController {
+    
+    var imageURL = "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20220607_129%2F16545872892918GA4h_JPEG%2Fmovie_image.jpg"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 컬렉션 뷰의 셀 크기, 셀 사이 간격 등 설정 (Estimate Size -> None으로 설정!)
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 4)
+        layout.itemSize = CGSize(width: width / 3, height: (width / 3) * 1.2)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
 
 
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommandCollectionViewCell", for: indexPath) as! RecommandCollectionViewCell
+        cell.posterImgView.backgroundColor = .orange
+        
+        let url = URL(string: imageURL)
+        cell.posterImgView.kf.setImage(with: url)
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        view.makeToast("\(indexPath.item)번째 셀을 선택했습니다.", duration: 1, position: .center)
+    }
 
 }
